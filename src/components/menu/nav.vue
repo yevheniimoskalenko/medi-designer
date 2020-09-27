675c1f916baeb077d91a63eeaa4a35747b8db49f
yevheniimoskalenko <testjokerqwerty@gmail.com>
2020-09-27 18:39:00 +0300

    fix adaptive 1

--- src/components/menu/nav.vue
+++ src/components/menu/nav.vue
@@ -75,19 +75,42 @@
   }
   .btn-link {
     text-decoration: none;
-    display: block;
+  }
+  .sign {
+    button {
+      display: block;
+    }
   }
 }
 
 @media (min-width: 576px) {
+  .btn-link {
+    text-decoration: none;
+  }
+  .sign {
+    button {
+      display: block;
+    }
+  }
 }
 
 // Medium devices (tablets, 768px and up)
 @media (min-width: 768px) {
+  .btn-link {
+    text-decoration: none;
+  }
+  .sign {
+    button {
+      display: block;
+    }
+  }
 }
 
 // Large devices (desktops, 992px and up)
 @media (min-width: 992px) {
+  .btn-link {
+    text-decoration: none;
+  }
 }
 
 // Extra large devices (large desktops, 1200px and up)
